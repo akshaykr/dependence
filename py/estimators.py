71bfe8cabba0000b4746dbb6863a4b75514113f0
akshaykr <akshaykr@cs.cmu.edu>
2014-01-21 17:29:10 -0500

    implemented quadratic estimator, and some plotting functionality

--- py/estimators.py
+++ py/estimators.py
@@ -1,5 +1,5 @@
 import numpy as np
-import kde, density
+import kde, density, lattice
 import itertools
 from helper import *
 
@@ -68,9 +68,14 @@ class LinearEstimator(PluginEstimator):
         self.p_est_data = pdata[num_pdata/2 + 1: num_pdata, :];
         self.q_est_data = qdata[num_qdata/2 + 1: num_qdata, :];
 
-    def eval(self):
+    def eval(self, fast=False):
         # Plugin estimator
-        plugin_est = super(LinearEstimator, self).eval();
+        plugin_est = super(LinearEstimator, self).eval(fast=fast);
+
+        if fast:
+            integrator = lambda x, y, z: fast_integration(x,y,z)
+        else:
+            integrator = lambda x, y, z: numeric_integration(x,y,z)
 
         # C1 = - (alpha + beta) * int {p0(x)^alpha q0(x)^beta} dx
         if self.dim == 1:
@@ -88,7 +93,7 @@ class LinearEstimator(PluginEstimator):
                                    1) ), self.beta) );
           l_limit = [0, 0];
           u_limit = [1, 1];
-        C1 = -(self.alpha + self.beta) * numeric_integration(
+        C1 = -(self.alpha + self.beta) * integrator(
                                            C1_fn_handle, l_limit, u_limit);
 
         # theta^p_{1,1} = alpha * E_{x~p}[p0(x)^(alpha-1) * q0(x)^beta]
@@ -106,8 +111,96 @@ class LinearEstimator(PluginEstimator):
         return plugin_est + theta_p_11 + theta_q_11 + C1;
 
 
-class QuadraticEstimator(object):
-    pass
+class QuadraticEstimator(PluginEstimator):
+    def __init__(self, pdata, qdata, alpha, beta, s):
+        # Shuffle the data and split it into 2 for density estimation and
+        # estimating the other parts.
+        np.random.shuffle(pdata);
+        np.random.shuffle(qdata);
+        num_pdata = pdata.shape[0];
+        num_qdata = qdata.shape[0];
+        # Call the previous constructor
+        PluginEstimator.__init__(self, pdata[0:num_pdata/2, :],
+          qdata[0:num_qdata/2, :], alpha, beta, s);
+        # Store the 2 splits
+        self.p_den_data = pdata[0: num_pdata/2, :];
+        self.q_den_data = qdata[0: num_qdata/2, :];
+        self.p_est_data = pdata[num_pdata/2 + 1: num_pdata, :];
+        self.q_est_data = qdata[num_qdata/2 + 1: num_qdata, :];
+        self.m = np.power(18*self.dim/s * np.power(2, 4.0*s/self.dim)* num_pdata**(-2), -float(self.dim)/(4*s+self.dim))
+
+    def eval(self, fast=True):
+        # Plugin estimator
+        plugin_est = super(QuadraticEstimator, self).eval(fast=fast);
+
+        if fast:
+            integrator = lambda x, y, z: fast_integration(x,y,z)
+        else:
+            integrator = lambda x, y, z: numeric_integration(x,y,z)
+
+        # C2 = 1/2(alpha(alpha-1) + alpha beta + beta(beta-1)) * int {p0(x)^alpha q0(x)^beta} dx
+        if self.dim == 1:
+          C2_fn_handle = lambda x: np.multiply( 
+            np.power(self.Kp.eval(np.matrix(x)), self.alpha),
+            np.power(self.Kq.eval(np.matrix(x)), self.beta) );
+          l_limit = [0];
+          u_limit = [1];
+
+        elif self.dim == 2:
+          C2_fn_handle = lambda x,y : np.multiply( 
+            np.power(self.Kp.eval( np.concatenate((np.matrix(x), np.matrix(y)),
+                                   1) ), self.alpha),
+            np.power(self.Kq.eval( np.concatenate((np.matrix(x), np.matrix(y)),
+                                   1) ), self.beta) );
+          l_limit = [0, 0];
+          u_limit = [1, 1];
+        C2 = 0.5 * (self.alpha*(self.alpha-1) + self.alpha*self.beta + self.beta*(self.beta-1)) * integrator(C2_fn_handle, l_limit, u_limit);
+
+        # theta^p_{2,1} = \alpha(2-\alpha+\beta/2) \EE[\phat^{\alpha-1}(X)\qhat^\beta(X)]
+        theta_p_21 = self.alpha*(2-self.alpha+self.beta/2)*np.mean(
+            np.multiply(np.power(self.Kp.eval(self.p_est_data), self.alpha-1),
+                        np.power(self.Kq.eval(self.p_est_data), self.beta))
+            )
+
+        # theta^q_{2,1} = \beta (2 - \beta+\alpha/2) \EE[\phat^\alpha(X)\qhat^{\beta-1}(X)]
+        theta_q_21 = self.beta*(2-self.beta+self.alpha/2)*np.mean(
+            np.multiply(np.power(self.Kp.eval(self.q_est_data), self.alpha),
+                        np.power(self.Kq.eval(self.q_est_data), self.beta-1))
+            )
+
+        # theta^{p,q}_{2,2} = 1/2 \alpha \beta \int \phat^{\alpha-1} \qhat^{\beta-1} pq
+        theta_pq_22 = np.sum([
+                np.mean(self.comp_exp(fn,self.p_est_data)) *
+                np.mean(np.array(self.comp_exp(fn, self.q_est_data)) *
+                        np.array(np.power(self.Kp.eval(self.q_est_data), self.alpha-1)) *
+                        np.array(np.power(self.Kq.eval(self.q_est_data), self.beta-1)))
+                for fn in lattice.lattice(self.dim, self.m)])
+
+        theta_pq_22 *= 0.5 * self.alpha * self.beta
+
+        # theta^p_{2,2} = 1/2 \alpha(\alpha-1) \int \phat^{\alpha-2}\qhat^\beta p^2
+        ## TODO: Not using the U-statistic here because I don't think it will be that much better.
+        ## Instead I'm using a related estimator as above
+        theta_p_22 = 0.5*self.alpha*(self.alpha-1) * np.sum([
+                np.mean(self.comp_exp(fn,self.p_est_data)) *
+                np.mean(np.array(self.comp_exp(fn, self.p_est_data)) *
+                        np.array(np.power(self.Kp.eval(self.p_est_data), self.alpha-2)) *
+                        np.array(np.power(self.Kq.eval(self.p_est_data), self.beta)))
+                for fn in lattice.lattice(self.dim, self.m)])
+        
+
+        # theta^q_{2,2} = 1/2 \beta(\beta-1) \int \phat^{\alpha}\qhat^{\beta-2} q^2
+        theta_q_22 = 0.5 * self.beta*(self.beta-1) * np.sum([
+                np.mean(self.comp_exp(fn,self.q_est_data)) *
+                np.mean(np.array(self.comp_exp(fn, self.q_est_data)) * 
+                        np.array(np.power(self.Kp.eval(self.q_est_data), self.alpha)) * 
+                        np.array(np.power(self.Kq.eval(self.q_est_data), self.beta-2)))
+                for fn in lattice.lattice(self.dim, self.m)])
+
+        return np.real(plugin_est + theta_p_21 + theta_q_21 + theta_p_22 + theta_q_22 + theta_pq_22 + C2);
+
+    def comp_exp(self, fn, x):
+        return np.exp(2j*np.pi*np.matrix(fn)*np.matrix(x).T)
 
 class Truth(object):
     def __init__(self, p, q, alpha, beta):
@@ -115,6 +208,7 @@ class Truth(object):
         self.q = q
         self.alpha = alpha
         self.beta = beta
+        self.dim = self.p.d
 
     def eval(self):
         """
--- py/rates.py
+++ py/rates.py
@@ -26,7 +26,7 @@ def kde_rate_1D(D, ns, ps, iters=10):
     return (ns, ms, vs)
 
 
-def estimator_rate(Est, Dp, Dq, ns, alpha=1, beta=1, iters=10):
+def estimator_rate(Est, Dp, Dq, ns, alpha=1, beta=1, iters=10, fast=True):
     """
     Compute the rate of convergenc of the estimator Est on the densities Dp, Dq
     The rate will be computed in \ell_1 norm.
@@ -42,7 +42,7 @@ def estimator_rate(Est, Dp, Dq, ns, alpha=1, beta=1, iters=10):
             pdata = Dp.sample(n)
             qdata = Dq.sample(n)
             E = Est(pdata, qdata, alpha, beta, Dp.s)
-            val = E.eval()
+            val = E.eval(fast=fast)
             sub_scores.append(np.abs(val-truth))
 
         ms.append(np.mean(sub_scores))
@@ -50,6 +50,28 @@ def estimator_rate(Est, Dp, Dq, ns, alpha=1, beta=1, iters=10):
         print "n = %d, av_er = %0.2f, std_er = %0.4f" % (n, np.mean(sub_scores), np.std(sub_scores))
     return (ns, ms, vs)
 
+
+def plot_estimator_rate(ns, ms, vs, gamma):
+    """
+    Generate three plots. 
+    One is an error bar plot of the error of the estimator.
+    The next is n vs \exp\{\log(err(n)) + \gamma \log n\} which should pull out the constant in the rate.
+    The third is n vs -\log(err(n)/\log(n) which should approach \gamma
+    """
+    fig = plt.figure(figsize=(15, 5))
+    ax1 = fig.add_subplot(131)
+    ax1.errorbar(ns, ms, vs)
+    ax1.set_xlabel("Number of samples (n)")
+    ax1.set_ylabel("Error |That - T|")
+    ax2 = fig.add_subplot(132)
+    ax2.plot(ns, [ms[i]*ns[i]**gamma for i in range(len(ns))])
+    ax2.set_xlabel("Number of samples (n)")
+    ax2.set_ylabel("Error*n^{\gamma}")
+    ax3 = fig.add_subplot(133)
+    ax3.plot(ns, [-np.log(ms[i])/np.log(ns[i]) for i in range(len(ns))])
+    ax3.set_xlabel("Number of samples (n)")
+    ax3.set_ylabel("-log(error)/log(n)")
+    plt.show()
 
 # To rescale a rate of n^{-\gamma}, plot ns versus 
 # [np.exp(np.log(ms[i]) + gamma*np.log(ns[i])) for i in range(len(ns))]
