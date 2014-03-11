db8f670fc9654edb0b594bdd1483c972a855f2e0
samy <kandasamy@cmu.edu>
2014-03-10 21:31:50 -0400

    added classification to v2

---
+++ l2/common/computeL2DistanceMatrix.m
@@ -0,0 +1,23 @@
+function [D] = computeL2DistanceMatrix(data)
+% data is a cell array containing num_dists matrices. Each matrix of size
+% nixnum_dims gives the points sampled from each distribution.
+
+  % Prelims
+  num_dists = numel(data);
+  
+  % First compute the similarity matrix
+  D = zeros(num_dists);
+  for i = 1:num_dists
+    if (num_dists > 40) && (mod(i,10) == 0)
+      % then report progress
+      fprintf('Computing L2 between %dth distribution and others\n', i);
+    end
+    for j = (i+1):num_dists
+      l2ij = l2Divergence(data{i}, data{j});
+      if imag(l2ij) > 0, l2ij = 0;
+      end
+      D(i,j) = l2ij;
+      D(j,i) = l2ij;
+    end
+  end
+end
--- l2/common/distributionClustering.m
+++ l2/common/distributionClustering.m
@@ -9,20 +9,9 @@ function [labels, A] = distributionClustering(data, num_clusters, bandwidth)
   % Prelims
   num_dists = numel(data);
   
-  % First compute the similarity matrix
-  A = zeros(num_dists);
-  for i = 1:num_dists
-    if (num_dists > 40) && (mod(i,10) == 0)
-      % then report progress
-      fprintf('Computing L2 between %dth distribution and others\n', i);
-    end
-    for j = (i+1):num_dists
-      l2ij = l2Divergence(data{i}, data{j});
-      k2ij = exp(-l2ij^2/ (2*bandwidth^2) );
-      A(i,j) = k2ij;
-      A(j,i) = k2ij;
-    end
-  end
+  % First compute the distance Matrix
+  D = computeL2DistanceMatrix(data);
+  A = exp(-D.^2/ (2*bandwidth^2));
 
   % Finally perform Spectral Clustering
   labels = spectralCluster(A, num_clusters);
